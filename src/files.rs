e51c1865e38ca465079fe88495a49c75f25394be
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2019-10-18 16:55:59 +0200

    renaming to uppercase the statics

--- src/files.rs
+++ src/files.rs
@@ -1,4 +1,4 @@
-static default_config: &'static str = r#"
+static DEFAULT_CONFIG_CONTENT: &'static str = r#"
 debug = false
 
 [global]
@@ -13,6 +13,6 @@ use std::path::Path;
 pub fn create_default_config() -> std::io::Result<()> {
     let path = Path::new(super::settings::DEFAULT_CONF_FILE);
     let mut file_w = File::create(path.display().to_string())?;
-    file_w.write_all(default_config.as_bytes())?; 
+    file_w.write_all(DEFAULT_CONFIG_CONTENT.as_bytes())?; 
     Ok(())
 }
