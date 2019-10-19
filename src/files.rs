0c942f7a02fba181f4fab4ab269f5966e993d49a
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2019-10-19 22:55:00 +0200

    Added function to validate path and create them if not exists

--- src/files.rs
+++ src/files.rs
@@ -7,7 +7,7 @@ content_root_path = "topics"
 "#;
 
 use std::io::prelude::*;
-use std::fs::File;
+use std::fs::{File, create_dir_all};
 use std::path::Path;
 
 pub fn create_default_config() -> std::io::Result<()> {
@@ -15,4 +15,28 @@ pub fn create_default_config() -> std::io::Result<()> {
     let mut file_w = File::create(path.display().to_string())?;
     file_w.write_all(DEFAULT_CONFIG_CONTENT.as_bytes())?; 
     Ok(())
+}
+
+pub fn validate_path(path: &Path, kind: &str) -> std::io::Result<()> {
+    if !&path.exists() {
+        println!("\tPath does not exist: {}, creating...", &path.display());
+        match &kind[..] {
+            "folder" => {
+                create_dir_all(&path)?;
+                Ok(())
+            },
+            "file" => {
+                let file = format!("{}{}", &path.display().to_string(), ".md".to_string());
+                File::create(file)?;
+                Ok(())
+            },
+            _ => { 
+                panic!("Error, {} type does not exists...", kind);
+            }
+        }
+    }
+    else {
+        println!("Path {} of type {} already exists", path.display(), kind);
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,10 @@ fn main() {
     //let mut buffer = String::new();
     println!("Reading config file...");
     let settings = Settings::new().unwrap();
-    println!("{:?}", settings);
+    println!("You have all of this topics at {}/{}:\n",
+        settings.global.repository_path, 
+        settings.global.content_root_path);
+    println!("Which topic you want to Add/Modify: ");
 
     //match io::stdin().read_line(&mut buffer) {
     //    Ok(_) => files::find_and_read(&buffer),
